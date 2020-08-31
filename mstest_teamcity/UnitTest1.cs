41d56631f6ed1129013b2d1cba4c9af4604ff2b1
widebyte <rafaelsilvabarbosabrbs73@gmail.com>
2020-08-30 22:00:18 -0300

    Primeiro Commit

---
+++ mstest_teamcity/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace mstest_teamcity
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
